a4909a216f7dd1bfab689028eac7e1ca899678ee
emhhd7 <eharpermarie@gmail.com>
2021-06-25 10:29:21 -0400

    first commit

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,10 +1,9 @@
-CREATE TABLE rankings {
-    id serial PRIMARY KEY,
-    ranking_title text NOT NULL
-}
-
-CREATE TABLE topics {
-    id serial PRIMARY KEY,
+CREATE TABLE rankings (
+    id serial primary key,
+    ranking_title text NOT NULL 
+);
+CREATE TABLE topics (
+    id serial primary key,
     topic_name text NOT NULL,
-    topic_score INTEGER REFERENCES rankings (id)
-};
+    topic_score integer REFERENCES rankings(id)
+);
