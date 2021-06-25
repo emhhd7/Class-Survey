cc1bc0c371a35d3dc2a7d0d5026b830c7d2cb0bb
emhhd7 <eharpermarie@gmail.com>
2021-06-25 12:41:19 -0400

    error, files

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const port = 3000
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 const es6Renderer = require('express-es6-template-engine')
 
 app.engine('html', es6Renderer)
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,55 @@ const ClassSurveyModel = require('../models/ClassSurveyModel')
 
 router.get('/', async (req, res) => {
     const topicData = await ClassSurveyModel.getAllTopicData();
-    console.log("TOPIC DATA", topicData)
+    const rankings = await ClassSurveyModel.getAllRankings();
+    // console.log("RANKINGS ARE: ", rankings)
     res.render('template', {
         locals: {
             title: 'Class Survey',
-            data: topicData
+            data: topicData,
+            rankings: rankings
         },
         partials: {
             body: 'partials/home'
         }
     })
+})
+
+router.get('/error', async (req, res) => {
+    const { error } = req.query;
+
+    let errorMessage = error;
+
+    if (!error) {
+        errorMessage = "No Error Message Defined";
+    }
+
+    res.render('template', {
+        locals: {
+            title: "ERROR PAGE",
+            errorMessage: errorMessage
+        },
+        partials: {
+            body: 'partials/error'
+        }
+    })
+})
+
+
+router.post('/update', async (req, res) => {
+    let response;
+
+    for (let key in req.body) {
+        console.log("KEY AND VALUE: ", key, req.body[key])
+        response = await ClassSurveyModel.updateRanking(key, req.body[key])
+    }
+
+    if (response.rowCount !== 1) {
+        res.redirect(`/error?error=${response}`)
+    } else {
+        res.redirect('/')
+    }
+
 })
 
 module.exports = router;
